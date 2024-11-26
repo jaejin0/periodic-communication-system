d84ee19b0e85ceb4605bb1213b8eab49737454d9
jaejin <jaejin0109@gmail.com>
2024-11-26 03:00:29 -0600

    initialized main file with comments on the project description

---
+++ main.py
@@ -0,0 +1,4 @@
+'''
+This work investigates the use of frequency domain to optimize periodic communication problem in multi-robot system.
+The system consists of robots where each robot follows a circular path, and may meet other robots if they have rendezvous point
+'''
