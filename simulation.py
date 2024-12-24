4f842a33faa7acab243f64323a986e11ae806cb1
jaejin <jaejin0109@gmail.com>
2024-12-23 21:32:22 -0600

    modified transition function

--- simulation.py
+++ simulation.py
@@ -13,19 +13,39 @@ class Simulation:
         self.rendezvous = rendezvous # key: (i, j), value: [angle, angle] 
         
         self.robot_num = len(circumferences)
-        # robots : [current_velocity, current_angle]
-        self.robots = [[0, initial_angles[i]] for i in self.robot_num] 
-        
+        self.initial_velocity = 1 
+        # robots : [current velocity, current angle, holding packet ids]
+        self.robots = [[self.initial_velocity, initial_angles[i], []] for i in self.robot_num] 
+     
+        self.timestep = 0
+        self.staleness = 0
+        self.packet_id = 0
+        self.packets = [] # [[timestep at src, timestep at dest]] 
+
     def reset:
         for i in range(self.robot_num):
-            self.robots[i][0] = 0 # current_velocity
+            self.robots[i][0] = self.initial_velocity # current_velocity
             self.robots[i][1] = self.initial_angles[i] # current_angle
 
     def step:
-        # calculate the diameter and change the angle of the robot
+        self.action()        
+        self.transition()
 
-    def transition:
+    def action:
+        # set velocity if this simulation provides the action choice per timestep
         pass
+
+    def transition:          
+  
+        # calculate the diameter and the angle derivative
+        # check if there is any packet sharing between robots
+        # check if the robot of src index passed src angle
+        # check if the robot of dest index passed 
+        
+        # change the angle of the robot
+       
+    def passing_src:
+        
 
     def get_staleness:
         # largest shift from src to dest
