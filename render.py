b34cb9d1f659e04ec6abad70954a970646b7d157
jaejin <jaejin0109@gmail.com>
2025-01-01 21:34:34 -0600

    created the center of the circular move and calculated the angle of the robot

--- render.py
+++ render.py
@@ -1,15 +1,25 @@
 import pygame
+import math
+
+def move_circle():
+    speed = 1 # degree / timestep
+    
+    current_angle = math.atan2(player.y - center[1], player.x - center[0])
+    print(current_angle)
 
 pygame.init()
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 player = pygame.Rect((300, 250, 50, 50))
+center = pygame.Rect((300, 200, 10, 10))
 
 running = True
 while running:
     screen.fill("white")
-    pygame.draw.circle(screen, (255, 0, 0), (player.x, player.y), 30)
+    pygame.draw.circle(screen, (255, 0, 0), (player.x, player.y), 10)
+    pygame.draw.circle(screen, (0, 0, 255), (center.x, center.y), 4)
+    move_circle()
 
     key = pygame.key.get_pressed()
     if key[pygame.K_a] == True:
@@ -28,3 +38,5 @@ while running:
     pygame.display.update()
 
 pygame.quit()
+
+
