2829ccf4b0c74c845c3d7706b36d42dd26783577
jaejin <jaejin0109@gmail.com>
2025-01-02 23:38:50 -0600

    implemented checking in sensor range feature

--- render.py
+++ render.py
@@ -17,14 +17,17 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 # robot properties
+robot_radius = 5
+robot_sensor_range = 10
+
 robot_num = 2
 robots = [{} for i in range(robot_num)]
     # robot 0
 robots[0]["center_x"] = 300
 robots[0]["center_y"] = 250
 robots[0]["radius"] = 50
-robots[0]["angle"] = 3.1
-robots[0]["angular_speed"] = 0.03
+robots[0]["angle"] = 0
+robots[0]["angular_speed"] = 0.00
     # robot 1
 robots[1]["center_x"] = 400
 robots[1]["center_y"] = 250
@@ -44,7 +47,8 @@ while running:
     # clear screen
     screen.fill("white")
 
-    # render objects 
+    # render objects
+    current_positions = []
     for i in range(robot_num):
         # render circumference
         pygame.draw.circle(screen, BLUE, (robots[i]["center_x"], robots[i]["center_y"]), robots[i]["radius"], 2) 
@@ -52,8 +56,14 @@ while running:
         # render robot
         x = robots[i]["center_x"] + robots[i]["radius"] * math.cos(robots[i]["angle"])
         y = robots[i]["center_y"] + robots[i]["radius"] * math.sin(robots[i]["angle"])
-        pygame.draw.circle(screen, RED, (x, y), 5)
-    
+        pygame.draw.circle(screen, RED, (x, y), robot_radius)
+
+        # check if they met
+        for _x, _y in current_positions:
+            if math.sqrt((x - _x)**2 + (y - _y) **2) <= robot_sensor_range:
+                pygame.draw.circle(screen, GREEN, (x, y), robot_sensor_range, 2)
+        current_positions.append([x, y])
+
    # action choice
     key = pygame.key.get_pressed()
     if key[pygame.K_a] == True:
