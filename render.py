03d8b99655d71a9fc8cc32e72950aaf0595bef45
jaejin <jaejin0109@gmail.com>
2025-01-02 23:53:37 -0600

    added source and destination of packet delivery

--- render.py
+++ render.py
@@ -35,6 +35,15 @@ robots[1]["radius"] = 50
 robots[1]["angle"] = 0
 robots[1]["angular_speed"] = 0.05
 
+# packet properties
+src_id = 0
+dest_id = 1
+src_angle = 3.14
+dest_angle = 0
+
+src_size = 10
+dest_size = 10
+
 # game loop
 running = True
 clock = pygame.time.Clock()
@@ -52,17 +61,30 @@ while running:
     for i in range(robot_num):
         # render circumference
         pygame.draw.circle(screen, BLUE, (robots[i]["center_x"], robots[i]["center_y"]), robots[i]["radius"], 2) 
-        
+
+        # render src and dest
+        src_x = robots[src_id]["center_x"] + robots[src_id]["radius"] * math.cos(src_angle)
+        src_y = robots[src_id]["center_y"] + robots[src_id]["radius"] * math.sin(src_angle)
+        dest_x = robots[dest_id]["center_x"] + robots[dest_id]["radius"] * math.cos(dest_angle)
+        dest_y = robots[dest_id]["center_y"] + robots[dest_id]["radius"] * math.sin(dest_angle)
+               
+        pygame.draw.rect(screen, GREEN, pygame.Rect(src_x - src_size / 2, src_y - src_size / 2, src_size, src_size))
+        pygame.draw.rect(screen, BLUE, pygame.Rect(dest_x - dest_size / 2, dest_y - dest_size / 2, dest_size, dest_size))
+
+
         # render robot
         x = robots[i]["center_x"] + robots[i]["radius"] * math.cos(robots[i]["angle"])
         y = robots[i]["center_y"] + robots[i]["radius"] * math.sin(robots[i]["angle"])
         pygame.draw.circle(screen, RED, (x, y), robot_radius)
 
         # check if they met
-        for _x, _y in current_positions:
+        for i, (_x, _y) in enumerate(current_positions):
             if math.sqrt((x - _x)**2 + (y - _y) **2) <= robot_sensor_range:
                 pygame.draw.circle(screen, GREEN, (x, y), robot_sensor_range, 2)
+        
         current_positions.append([x, y])
+
+
 
    # action choice
     key = pygame.key.get_pressed()
