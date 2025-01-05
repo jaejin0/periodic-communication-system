30db0b378c57da89c7d06612ac385f059a0fc6bc
jaejin <jaejin0109@gmail.com>
2025-01-04 23:59:05 -0600

    modified the source code to use the class object to run the simulation

--- render.py
+++ render.py
@@ -2,8 +2,8 @@ import pygame
 import math
 
 def get_robot_position(index):
-    x = robots[index]["center_x"] + robots[index]["radius"] * math.cos(robots[index]["angle"])
-    y = robots[index]["center_y"] + robots[index]["radius"] * math.sin(robots[index]["angle"])
+    x = robots[index].center_x + robots[index].radius * math.cos(robots[index].angle)
+    y = robots[index].center_y + robots[index].radius * math.sin(robots[index].angle)
     return x, y 
 
 # initialization
@@ -21,7 +21,7 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-class robot:
+class Robot:
     def __init__(
             self,
             center_x,
@@ -39,26 +39,28 @@ class robot:
         self.rendezvous = rendezvous
         self.previous_angle = None
 
-        # robot properties
-        robot_radius = 5
-        robot_max_velocity = 0.1
+# robot properties
+robot_radius = 5
+robot_max_velocity = 0.1
 
 robot_num = 2
-robots = [{} for i in range(robot_num)]
-    # robot 0
-robots[0]["center_x"] = 300
-robots[0]["center_y"] = 250
-robots[0]["radius"] = 50
-robots[0]["angle"] = 1.0
-robots[0]["angular_velocity"] = 0.01
-robots[0]["rendezvous"] = [[0, 1]] # List[angle, id]
-    # robot 1
-robots[1]["center_x"] = 400
-robots[1]["center_y"] = 250
-robots[1]["radius"] = 50
-robots[1]["angle"] = 0
-robots[1]["angular_velocity"] = 0.05
-robots[1]["rendezvous"] = [[float("{:.4f}".format(math.pi)), 0]]
+robots = []
+# robot 0
+robots.append(Robot(
+    center_x = 300,
+    center_y = 250,
+    radius = 50,
+    angle = 1.0,
+    angular_velocity = 0.01,
+    rendezvous = [[0, 1]]))
+# robot 1
+robots.append(Robot(
+    center_x = 400,
+    center_y = 250,
+    radius = 50,
+    angle = 1.0,
+    angular_velocity = 0.05,
+    rendezvous = [[float("{:.4f}".format(math.pi)), 0]]))
 
 # packet properties
 src_id = 0
@@ -84,13 +86,13 @@ while running:
     # render objects
     for i in range(robot_num):
         # render circumference
-        pygame.draw.circle(screen, BLUE, (robots[i]["center_x"], robots[i]["center_y"]), robots[i]["radius"], 2) 
+        pygame.draw.circle(screen, BLUE, (robots[i].center_x, robots[i].center_y), robots[i].radius, 2) 
 
         # render src and dest
-        src_x = robots[src_id]["center_x"] + robots[src_id]["radius"] * math.cos(src_angle)
-        src_y = robots[src_id]["center_y"] + robots[src_id]["radius"] * math.sin(src_angle)
-        dest_x = robots[dest_id]["center_x"] + robots[dest_id]["radius"] * math.cos(dest_angle)
-        dest_y = robots[dest_id]["center_y"] + robots[dest_id]["radius"] * math.sin(dest_angle)
+        src_x = robots[src_id].center_x + robots[src_id].radius * math.cos(src_angle)
+        src_y = robots[src_id].center_y + robots[src_id].radius * math.sin(src_angle)
+        dest_x = robots[dest_id].center_x + robots[dest_id].radius * math.cos(dest_angle)
+        dest_y = robots[dest_id].center_y + robots[dest_id].radius * math.sin(dest_angle)
                
         pygame.draw.rect(screen, GREEN, pygame.Rect(src_x - src_size / 2, src_y - src_size / 2, src_size, src_size))
         pygame.draw.rect(screen, BLUE, pygame.Rect(dest_x - dest_size / 2, dest_y - dest_size / 2, dest_size, dest_size))
@@ -103,24 +105,24 @@ while running:
         # if (i, robots[i]["angle"]) in rendezvous:
     # action choice
     key = pygame.key.get_pressed()
-    if key[pygame.K_a] == True and robots[0]["angular_velocity"] <= robot_max_velocity - 0.01:
-        robots[0]["angular_velocity"] += 0.01
-    elif key[pygame.K_d] == True and robots[0]["angular_velocity"] >= -robot_max_velocity + 0.01:
-        robots[0]["angular_velocity"] -= 0.01
+    if key[pygame.K_a] == True and robots[0].angular_velocity <= robot_max_velocity - 0.01:
+        robots[0].angular_velocity += 0.01
+    elif key[pygame.K_d] == True and robots[0].angular_velocity >= -robot_max_velocity + 0.01:
+        robots[0].angular_velocity -= 0.01
         
     # transition
     for i in range(robot_num):
-        current_angle = robots[i]["angle"]
-        robots[i]["angle"] += robots[i]["angular_velocity"]
-        robots[i]["angle"] = float("{:.4f}".format(robots[i]["angle"]))
+        current_angle = robots[i].angle
+        robots[i].angle += robots[i].angular_velocity
+        robots[i].angle = float("{:.4f}".format(robots[i].angle))
         # check if the robots met
-        for angle, j in robots[i]["rendezvous"]:
-            if robots[i]["angle"] >= angle and angle >= current_angle:
+        for angle, j in robots[i].rendezvous:
+            if robots[i].angle >= angle and angle >= current_angle:
                 x, y = get_robot_position(i)
                 pygame.draw.circle(screen, GREEN, (x, y), 20)
                  
-        if robots[i]["angle"] >= math.pi or robots[i]["angle"] <= -math.pi:
-            robots[i]["angle"] = -robots[i]["angle"]
+        if robots[i].angle >= math.pi or robots[i].angle <= -math.pi:
+            robots[i].angle = -robots[i].angle
         
 
 
