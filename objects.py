0162085a0c87f3537cc21e2a31767a698708bc63
jaejin <jaejin0109@gmail.com>
2025-01-11 18:53:16 -0600

    moved Robot class in objects.py file

---
+++ objects.py
@@ -0,0 +1,38 @@
+class Robot:
+    def __init__(
+            self,
+            center_x,
+            center_y,
+            center_radius,
+            initial_angle,
+            initial_velocity,
+            rendezvous,
+            ):
+        # static state
+        self.center_x = center_x
+        self.center_y = center_y
+        self.center_radius = center_radius
+        self.initial_angle = initial_angle
+        self.initial_angular_velocity = initial_angular_velocity
+        self.rendezvous = rendezvous
+        
+        # dynamic state 
+        self.angular_velocity = initial_angular_velocity
+        self.angle = initial_angle
+        self.previous_angle = None
+        
+        # default properties
+        self.robot_radius = 5
+        self.robot_max_velocity = 0.2
+   
+    def get_robot_position():
+        x = self.center_x + self.center_radius * math.cos(self.angle)
+        y = eslf.center_y + self.center_radius * math.sin(self.angle)
+        return x, y 
+
+    def transition():
+        self.previous_angle = robots[i].angle
+        self.angle += robots[i].angular_velocity
+        self.angle = float("{:.3f}".format(robots[i].angle))
+
+
