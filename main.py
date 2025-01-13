17d9934097ac760c819c8102ff6b1793633eec3c
jaejin <jaejin0109@gmail.com>
2025-01-12 22:18:57 -0600

    removed rendering functions in main.py

--- main.py
+++ main.py
@@ -39,34 +39,6 @@ robots.append(Robot(
     angular_velocity = 0.1,
     rendezvous = [[float("{:.3f}".format(math.pi)), 0]]))
 
-# game loop
-running = True
-clock = pygame.time.Clock()
-
-def render_circumference(i):
-    pygame.draw.circle(screen, BLUE, (robots[i].center_x, robots[i].center_y), robots[i].radius, 2) 
-
-def render_src():
-    src_x = robots[src_id].center_x + robots[src_id].radius * math.cos(src_angle)
-    src_y = robots[src_id].center_y + robots[src_id].radius * math.sin(src_angle)
-    pygame.draw.rect(screen, GREEN, pygame.Rect(src_x - src_size / 2, src_y - src_size / 2, src_size, src_size))
-
-def render_dest():
-    dest_x = robots[dest_id].center_x + robots[dest_id].radius * math.cos(dest_angle)
-    dest_y = robots[dest_id].center_y + robots[dest_id].radius * math.sin(dest_angle)
-    pygame.draw.rect(screen, BLUE, pygame.Rect(dest_x - dest_size / 2, dest_y - dest_size / 2, dest_size, dest_size))
-
-def render_robot(i):
-    x, y = robots[i].get_robot_position()
-    pygame.draw.circle(screen, RED, (x, y), robot_radius)
-
-def render():
-    for i in range(robot_num):
-        render_circumference(i)
-        render_src()
-        render_dest()
-        render_robot(i)
-
 def robot_met():
     # check if the robots met
     current_positions = []
@@ -82,22 +54,13 @@ def robot_met():
             robots[i].angle = -robots[i].angle
         
 
-
-
+# game loop
+running = True
+clock = pygame.time.Clock()
 while running: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
-    # clear screen
-    screen.fill("white")
-
-    # render objects
-    for i in range(robot_num):
-        render_circumference(i)
-        render_src()
-        render_dest()
-        render_robot(i)
 
         # check if they met
         # if (i, robots[i]["angle"]) in rendezvous:
@@ -113,6 +76,8 @@ while running:
         robots[i].transition()
 
     robot_met()
+
+    render()
 
     pygame.display.update()
     clock.tick(60)
