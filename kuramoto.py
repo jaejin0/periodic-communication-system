3e9dbf12da9af0dfe2605a5265f61879c745ff64
jaejin <jaejin0109@gmail.com>
2025-01-20 00:39:21 -0600

    created kuramoto class

--- kuramoto.py
+++ kuramoto.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 class Node:
-    def __init__(self, angle, angular_velocity):
+    def __init__(self, angle, natural_frequency):
         self.angle = angle
+        self.natural_frequency = natural_frequency
         self.angular_velocity = angular_velocity
 
+    def  
 
-
-    def update_angle():
-        self.angle += self.angular_velocity
+    def update_angle(self, derivative):
+        self.angle += derivative
         self.limit_angle() 
 
     def limit_angle():
@@ -16,3 +17,12 @@ class Node:
         self.angle = float("{:.4f}".format(self.angle))
         if self.angle > np.pi or self.angle < np.pi:
             self.angle *= -1
+
+class Kuramoto:
+    def __init__(self, N=30, K=1): 
+        self.N = N
+        self.K = K
+        self.angles = 2 * np.pi * np.random.normal(size=N)
+        self.natural_frequencies = np.random.normal(size=N)
+
+
