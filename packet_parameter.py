009211d237012006e95e98483cc249a23978d63a
jaejin <jaejin0109@gmail.com>
2025-01-25 18:37:30 -0600

    created function for finding angle

--- packet_parameter.py
+++ packet_parameter.py
@@ -24,40 +24,55 @@ class Node:
         self.K = K
         self.center = (center_x, center_y)
         self.radius = radius
-
-        # updated with 
+        self.packets = 0 # as packets intended to flow in one direction, counting packets work fine without storing packet ids
+        
+        # updated with add_neighbor function 
         self.neighbors = []
         self.rendezvous = [] # can be calculated using neighbor node's center and radius, but storing rendezvous angle reduce computation time
-        
-        self.packets = 0 # as packets intended to flow in one direction, counting packets work fine without storing packet ids
-    
+        self.adj_mat = nx.to_numpy_array(nx.erdoes_renyi_graph(n=0, p=1)) 
+
+        # configured for rendering 
+        self.circle_thickness = 2
+        self.particle_radius = 5
+
     def add_neighbor(self, neighbor: Node):
-        self.neighbors.append(neighbor)
-        self.rendezvous.append(self.compute_rendezvous(neighbor))
-
-    def compute_rendezvous(self, neighbor: Node):
+        epsilon = 3
         center_to_center_distance = math.sqrt(((self.center[0] - neighbor.center[0])**2) + ((self.center[1] - neighbor.center[1])**2)) 
-        if center_to_center_distance <= self.radius + neighbor.radius:
+        # if center_to_center_distance <= self.radius + neighbor.radius:
             # two rendezvous points
-        elif center_to_center_distance == self.radius + neighbor.radius:
+        if abs(center_to_center_distance - (self.radius + neighbor.radius)) <= epsilon:
             # one rendezvous points
+            self.rendezvous.append(np.arctan2(self.center[1] - neighbor.center[1], self.center[0] - neighbor.center[0])
         else:
-            # no rendezvous point
+            # no rendezvous point -> not a neighbor
+            return
 
+        self.neighbors.append(neighbor)
+    
+    def derivative1(self):
+        self.adj_mat = nx.to_numpy_array(nx.erdos_renyi_graph(n=self.new_packet_num(), p=1))
 
-class Communication:
-    def __init__(self, N1=1, N2=1, K=1): 
-        self.N1 = N1
-        self.N2 = N2
-        self.K = K
-        # random distribution of initial angles and natural frequencies 
-        self.angles1 = np.random.normal(loc=0, scale= 2 * np.pi, size=N1)
-        self.angles2 = np.random.normal(loc=0, scale= 2 * np.pi, size=N2)
-        self.natural_frequencies1 = np.random.normal(loc=0.02, scale=0.01, size=N1)  
-        self.natural_frequencies2 = np.random.normal(loc=0.02, scale=0.01, size=N2)
-        # a matrix representing connectivity between particles
-        self.adj_mat = nx.to_numpy_array(nx.erdos_renyi_graph(n=N1+N2, p=1)) 
+        angles = np.concatenate((self.angles1 + self.rendezvous1, self.angles2 + self.rendezvous2))
+        angles_i, angles_j = np.meshgrid(angles, angles)
+        interactions = self.adj_mat * np.sin(angles_j - angles_i)
+       
+        adjust_frequencies = self.K * interactions.sum(axis=0) / (self.N1 + self.N2)
+        derivative = self.natural_frequencies1 + adjust_frequencies[:self.N1] 
 
+        return derivative
+
+    def new_packet_num(self):
+        new_packet = 0
+        for n in self.neighbors:
+            if self.packets < n.packets:
+                new_packets += (n.packets - self.packets)
+        return new_packets
+
+    def find_angles(self):
+        
+
+class Simulation:
+    def __init__(self): 
         # game setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -65,35 +80,12 @@ class Communication:
         pygame.display.set_caption("Kuramoto-based Communication model simulation")
         self.timestep = pygame.time.get_ticks()
 
-        # configuration
-        self.radius1 = HEIGHT // 4
-        self.radius2 = HEIGHT // 4
-        self.center1 = ((WIDTH // 2) - ((self.radius1 + self.radius2) / 2), HEIGHT // 2)
-        self.center2 = ((WIDTH // 2) + ((self.radius1 + self.radius2) / 2), HEIGHT // 2)
-        self.rendezvous1 = 0
-        self.rendezvous2 = np.pi 
-        self.circle_thickness = 2
-        self.particle_radius = 5
-
     def step(self):
         self.angles1 += self.derivative1()
         self.angles2 += self.derivative2()
         self.render()
         pygame.display.update()
         self.clock.tick(60)
-    
-    def derivative1(self):
-        angles = np.concatenate((self.angles1 + self.rendezvous1, self.angles2 + self.rendezvous2))
-        angles_i, angles_j = np.meshgrid(angles, angles)
-        interactions = self.adj_mat * np.sin(angles_j - angles_i)
-       
-        adjust_frequencies = self.K * interactions.sum(axis=0) / (self.N1 + self.N2)
-        derivative = self.natural_frequencies1 + adjust_frequencies[:self.N1] 
-        
-        return self.natural_frequencies1
-
-        return derivative
-
     def derivative2(self):
         angles = np.concatenate((self.angles1 + self.rendezvous1, self.angles2 + self.rendezvous2))
         angles_i, angles_j = np.meshgrid(angles, angles)
